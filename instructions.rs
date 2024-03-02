2281af88c400b0ce196e002878ed40a837dcf3bb
Ignacio Moya <moyaignacioj@gmail.com>
2024-03-02 08:48:52 -0300

    Function prototypes for RV32I instruction set.

---
+++ instructions.rs
@@ -0,0 +1,49 @@
+pub mod registers;
+//RV32I instruction set functions
+
+pub fn add(){}
+pub fn sub(){}
+pub fn and(){}
+pub fn or(){}
+pub fn xor(){}
+pub fn sll(){}
+pub fn srl(){}
+pub fn sra(){}
+pub fn slt(){}
+pub fn sltu(){}
+
+pub fn addi(){}
+pub fn andi(){}
+pub fn ori(){}
+pub fn xori(){}
+pub fn slli(){}
+pub fn srli(){}
+pub fn srai(){}
+pub fn slti(){}
+pub fn sltiu(){}
+
+pub fn beq(){}
+pub fn bne(){}
+pub fn bge(){}
+pub fn bgeu(){}
+pub fn blt(){}
+pub fn bltu(){}
+pub fn jal(){}
+pub fn jalr(){}
+pub fn ecall(){}
+pub fn ebreak(){}
+
+pub fn lb(){}
+pub fn lbu(){}
+pub fn lh(){}
+pub fn lhu(){}
+pub fn lw(){}
+pub fn lui(){}
+pub fn auipc(){}
+pub fn fence(){}
+
+pub fn sb(){}
+pub fn sh(){}
+pub fn sw(){}
+
+// In the future, functions for extensions instructios will be defined below.
