8dcb12d34b8eb744d0e78310f8eabc328d839dc4
Ignacio Moya <moyaignacioj@gmail.com>
2024-03-11 13:46:46 -0300

    Implemented more instructions, move registers to instructions/ as a nested module.

--- instructions.rs
+++ instructions.rs
@@ -1,120 +1,267 @@
 pub mod registers;
 //RV32I instruction set functions
-
-pub fn add(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn add(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     registers::set(destination, operand1 + operand2);
 }
 
-pub fn sub(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn sub(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     registers::set(destination, operand1 - operand2);
 }
 
-pub fn and(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn and(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     registers::set(destination, operand1 & operand2);
 }
 
-pub fn or(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn or(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     registers::set(destination, operand1 | operand2);
 }
 
-pub fn xor(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn xor(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     registers::set(destination, operand1 ^ operand2);
 }
 
-pub fn sll(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn sll(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     // Logical shift left source1 by the value stored in the lowest 5 bits of source2.
-    register::set(destination, operand1 << (openrand2 & 0x20));
+    registers::set(destination, operand1 << (operand2 & 0x20));
 }
 
-pub fn srl(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn srl(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     // Logical shift right source1 by the value stored in the lowest 5 bits of source2.
-    register::set(destination, operand1 >> (openrand2 & 0x20));
+    registers::set(destination, operand1 >> (operand2 & 0x20));
 }
 
-pub fn sra(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn sra(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     // Arithmetic shift right source1 by the value stored in the lowest 5 bits of source2.
     //TODO: Rust seems to chose arithmetic or logic shift automatically. So I think both source needs to be signed.
-    register::set(destination, operand1 >> (openrand2 & 0x20));
+    registers::set(destination, operand1 >> (operand2 & 0x20));
 }
 
-pub fn slt(destination: i32, source1: i32, source2: i32){
+#[allow(dead_code)]
+pub fn slt(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     
-    let result = if(operand1 < openrand2){ 1 } else { 0};
-    register::set(destination, result);
+    let result = if operand1 < operand2 { 1 } else { 0};
+    registers::set(destination, result);
 }
 
-pub fn sltu(destination: i32, source1: u32, source2: u32){
+#[allow(dead_code)]
+pub fn sltu(destination: usize, source1: usize, source2: usize){
     let operand1 = registers::get(source1);
     let operand2 = registers::get(source2);
     
-    let result = if(operand1 < openrand2){ 1 } else { 0};
-    register::set(destination, result);
+    let result = if operand1 < operand2 { 1 } else { 0};
+    registers::set(destination, result);
 }
 
-pub fn addi(destination: i32, source1: i32, inmediate: i32){
+#[allow(dead_code)]
+pub fn addi(destination: usize, source1: usize, immediate: i32){
     let operand1 = registers::get(source1);
-    register::set(destination, operand1 + inmediate);
+    registers::set(destination, operand1 + immediate);
 }
 
-pub fn andi(destination: i32, source1: i32, inmediate: i32){
+#[allow(dead_code)]
+pub fn andi(destination: usize, source1: usize, immediate: i32){
     let operand1 = registers::get(source1);
-    register::set(destination, operand1 & inmediate);
+    registers::set(destination, operand1 & immediate);
 }
 
-pub fn ori(destination: i32, source1: i32, inmediate: i32){
+#[allow(dead_code)]
+pub fn ori(destination: usize, source1: usize, immediate: i32){
     let operand1 = registers::get(source1);
-    register::set(destination, operand1 | inmediate);
+    registers::set(destination, operand1 | immediate);
 }
-pub fn xori(destination: i32, source1: i32, inmediate: i32){
+
+#[allow(dead_code)]
+pub fn xori(destination: usize, source1: usize, immediate: i32){
     let operand1 = registers::get(source1);
-    register::set(destination, operand1 ^ inmediate);
+    registers::set(destination, operand1 ^ immediate);
 }
-pub fn slli(){}
-pub fn srli(){}
-pub fn srai(){}
-pub fn slti(){}
-pub fn sltiu(){}
 
-pub fn beq(){}
-pub fn bne(){}
-pub fn bge(){}
-pub fn bgeu(){}
-pub fn blt(){}
-pub fn bltu(){}
-pub fn jal(){}
-pub fn jalr(){}
-pub fn ecall(){}
-pub fn ebreak(){}
+#[allow(dead_code)]
+pub fn slli(destination: usize, source1: usize, immediate: i32){
+    let operand1 = registers::get(source1);
+    registers::set(destination, operand1 << (immediate & 0x20));
+}
 
+#[allow(dead_code)]
+pub fn srli(destination: usize, source1: usize, immediate: i32){
+    let operand1 = registers::get(source1);
+    registers::set(destination, operand1 >> (immediate & 0x20));
+}
+
+#[allow(dead_code)]
+pub fn srai(destination: usize, source1: usize, immediate: i32){
+    let operand1 = registers::get(source1);
+    registers::set(destination, operand1 >> (immediate & 0x20));
+}
+
+#[allow(dead_code)]
+pub fn slti(destination: usize, source1: usize, immediate: i32){
+    let operand1 = registers::get(source1);
+    
+    let result = if operand1 < immediate { 1 } else { 0};
+    registers::set(destination, result);
+}
+
+#[allow(dead_code)]
+pub fn sltiu(destination: usize, source1: usize, immediate: u32){
+    let operand1 = registers::get(source1) as u32;
+    
+    let result = if operand1 < immediate { 1 } else { 0};
+    registers::set(destination, result);
+}
+
+#[allow(dead_code)]
+pub fn beq(source1: usize, source2: usize, offset: u32){
+    let operand1 = registers::get(source1);
+    let operand2 = registers::get(source2);
+    if operand1 == operand2
+    {
+        unsafe{
+            registers::PROGRAM_COUNTER += offset;
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn bne(source1: usize, source2: usize, offset: u32){
+    let operand1 = registers::get(source1);
+    let operand2 = registers::get(source2);
+    if operand1 != operand2
+    {
+        unsafe{
+            registers::PROGRAM_COUNTER += offset;
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn bge(source1: usize, source2: usize, offset: u32){
+    let operand1 = registers::get(source1);
+    let operand2 = registers::get(source2);
+    if operand1 >= operand2
+    {
+        unsafe{
+            registers::PROGRAM_COUNTER += offset;
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn bgeu(source1: usize, source2: usize, offset: u32){
+    let operand1 = registers::get(source1);
+    let operand2 = registers::get(source2);
+    if operand1 == operand2
+    {
+        unsafe{
+            registers::PROGRAM_COUNTER += offset;
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn blt(source1: usize, source2: usize, offset: u32){
+    let operand1 = registers::get(source1);
+    let operand2 = registers::get(source2);
+    if operand1 < operand2
+    {
+        unsafe{
+            registers::PROGRAM_COUNTER += offset;
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn bltu(source1: usize, source2: usize, offset: u32){
+    let operand1 = registers::get(source1);
+    let operand2 = registers::get(source2);
+    if operand1 < operand2
+    {
+        unsafe{
+            registers::PROGRAM_COUNTER += offset;
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn jal(source1: usize, offset: u32){
+    unsafe{
+        registers::PROGRAM_COUNTER += offset;
+    }
+    registers::set(source1, registers::get(source1) + 4);
+}
+
+#[allow(dead_code)]
+pub fn jalr(){
+    //TODO
+}
+
+#[allow(dead_code)]
+pub fn ecall(){
+    //TODO: Raises EnvironmentCall
+}
+
+#[allow(dead_code)]
+pub fn ebreak(){
+    //TODO: Raises Breakpoint
+}
+
+#[allow(dead_code)]
 pub fn lb(){}
+
+#[allow(dead_code)]
 pub fn lbu(){}
+
+#[allow(dead_code)]
 pub fn lh(){}
+
+#[allow(dead_code)]
 pub fn lhu(){}
+
+#[allow(dead_code)]
 pub fn lw(){}
+
+#[allow(dead_code)]
 pub fn lui(){}
+
+#[allow(dead_code)]
 pub fn auipc(){}
+
+#[allow(dead_code)]
 pub fn fence(){}
 
+#[allow(dead_code)]
 pub fn sb(){}
+
+#[allow(dead_code)]
 pub fn sh(){}
+
+#[allow(dead_code)]
 pub fn sw(){}
 
 // In the future, functions for RISC ISA extensions will be defined below.
---
+++ instructions/registers.rs
@@ -0,0 +1,28 @@
+pub const REGISTER_LENGTH: i32 = 32;
+pub const REGISTER_WIDTH: i32 = 32;
+
+//TODO: Find a way to avoid having to use static mut
+static mut REGISTERS: [i32; 32] = [0; 32];
+
+pub static mut PROGRAM_COUNTER: u32 = 0;
+
+pub fn print(){
+    unsafe{
+        for reg in REGISTERS{
+            println!{"{:#032b}", reg};
+        }
+        println!("PROGRAM COUNTER: {:#032b}", PROGRAM_COUNTER);
+    }
+}
+
+pub fn set(index: usize, value: i32){
+    unsafe{
+        REGISTERS[index] = value;
+    }
+}
+
+pub fn get(index: usize) -> i32{
+    unsafe{
+        REGISTERS[index]
+    }
+}
